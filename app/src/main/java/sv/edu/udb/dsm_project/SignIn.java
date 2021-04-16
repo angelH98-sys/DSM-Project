6c67697b0d9b465fda72c14b17c9560f244068aa
angel <angelH98@outlook.com>
2021-04-15 23:25:35 -0600

    SignIn Layout 2

--- app/src/main/java/sv/edu/udb/dsm_project/SignIn.java
+++ app/src/main/java/sv/edu/udb/dsm_project/SignIn.java
@@ -2,7 +2,9 @@ package sv.edu.udb.dsm_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignIn extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void toSignUp(View v){
+
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/sv/edu/udb/dsm_project/SignUp.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.dsm_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
