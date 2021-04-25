072e63e4b3c27e5f888850a294cc76d0c3b136c8
RicardoSolorzano0 <ricardosolorzano97@gmail.com>
2021-04-24 18:14:49 -0600

    cargando datos

--- app/src/main/java/sv/edu/udb/dsm_project/Carrito.java
+++ app/src/main/java/sv/edu/udb/dsm_project/Carrito.java
@@ -1,18 +1,28 @@
 package sv.edu.udb.dsm_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Date;
+import java.util.List;
 
 public class Carrito extends AppCompatActivity {
 
     private TextView prueba;
     FirebaseFirestore mFirestore;
+    private String loginUser;
 
 
 
@@ -20,7 +30,8 @@ public class Carrito extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrito);
-
+        //quemado
+        loginUser="usuarioprueba123";
         mFirestore=FirebaseFirestore.getInstance();
         prueba=(TextView)findViewById(R.id.prueba);
 
@@ -32,16 +43,52 @@ public class Carrito extends AppCompatActivity {
     }
 
     private void obtenerDatos(){
-        mFirestore.collection("tickets").document("9El7wXakxioCqYlPHZPY").get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        mFirestore.collection("tickets").whereEqualTo("id_usuario",loginUser).whereEqualTo("estado","Borrador").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if(documentSnapshot.exists()){
-                    String estado = documentSnapshot.getString("estado");
-                    String id_usuario = documentSnapshot.getString("id_usuario");
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        String estado = document.getData().get("estado").toString();
+                        Date fechapago = (Date)document.getData().get("fechapago");
+                        String id_usuario = loginUser;
+                        double preciototal = (Double) document.getData().get("preciototal");
 
-                    prueba.setText(estado+"    "+id_usuario);
+
+                        /*Object[] productos = (Object[]) document.getData().get("productos");
+                        List<productosTicket>  productosEnTicket;
+                        for (Object o:productos){
+
+                        }*/
+                        String valor=document.getData().get("productos").toString();
+
+                        Log.d("return", document.getData().get("productos").toString());
+                        prueba.setText(estado + "    " + fechapago + "     " + id_usuario + "    " + preciototal+"\n"+document.getData().get("productos").toString()+"\n\n"+valor.substring(2));
+
+                    }
                 }
             }
         });
     }
+
+    public class productosTicket{
+        int cantidad;
+        String id_producto;
+        String nombre;
+        Double precio;
+        Double subtotal;
+
+        public void main(int cantidad,String id_producto,String nombre,Double precio,Double subtotal){
+            this.cantidad=cantidad;
+            this.id_producto=id_producto;
+            this.nombre=nombre;
+            this.precio=precio;
+            this.subtotal=subtotal;
+        }
+
+
+
+
+    }
+
 }
+
