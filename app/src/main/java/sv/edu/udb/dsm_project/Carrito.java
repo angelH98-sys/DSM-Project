a6db56f8ceeb870b3d46a447b01b5bfbd0d6c794
angel <angelH98@outlook.com>
2021-04-24 23:04:11 -0600

    Using Gson and JSONParser

--- app/src/main/java/sv/edu/udb/dsm_project/Carrito.java
+++ app/src/main/java/sv/edu/udb/dsm_project/Carrito.java
@@ -14,17 +14,22 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
 
 public class Carrito extends AppCompatActivity {
 
     private TextView prueba;
     FirebaseFirestore mFirestore;
     private String loginUser;
-
-
+    private List<productosTicket> productos = new ArrayList<productosTicket>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +40,6 @@ public class Carrito extends AppCompatActivity {
         mFirestore=FirebaseFirestore.getInstance();
         prueba=(TextView)findViewById(R.id.prueba);
 
-
-
-
-
         obtenerDatos();
     }
 
@@ -47,24 +48,37 @@ public class Carrito extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        String estado = document.getData().get("estado").toString();
-                        Date fechapago = (Date)document.getData().get("fechapago");
-                        String id_usuario = loginUser;
-                        double preciototal = (Double) document.getData().get("preciototal");
 
 
-                        /*Object[] productos = (Object[]) document.getData().get("productos");
-                        List<productosTicket>  productosEnTicket;
-                        for (Object o:productos){
+                    JSONParser parser = new JSONParser();
+                    Gson gson = new Gson();
+                    try{
+                        JSONObject obj, aux;
+                        JSONArray array;
 
-                        }*/
-                        String valor=document.getData().get("productos").toString();
+                        int cantidad;
+                        String id_producto, nombre;
+                        Double precio, subtotal;
+                        for (QueryDocumentSnapshot document : task.getResult()) {
 
-                        Log.d("return", document.getData().get("productos").toString());
-                        prueba.setText(estado + "    " + fechapago + "     " + id_usuario + "    " + preciototal+"\n"+document.getData().get("productos").toString()+"\n\n"+valor.substring(2));
+                            obj = (JSONObject) parser.parse(gson.toJson(document.getData()));
+                            array = (JSONArray) obj.get("productos");
+                            for (int i = 0; i <= array.size(); i++){
+                                aux = (JSONObject) array.get(i);
 
+                                cantidad = Integer.parseInt(aux.get("cantidad").toString());
+                                id_producto = aux.get("id_producto").toString();
+                                nombre = aux.get("nombre").toString();
+                                precio = Double.parseDouble(aux.get("precio").toString());
+                                subtotal = Double.parseDouble(aux.get("subtotal").toString());
+
+                                productos.add(new productosTicket(cantidad, id_producto, nombre, precio, subtotal));
+                            }
+                        }
+                    }catch (Exception e){
+                        Log.d("exception", e.toString());
                     }
+                    Log.d("TAG", "onComplete: ");
                 }
             }
         });
@@ -77,16 +91,13 @@ public class Carrito extends AppCompatActivity {
         Double precio;
         Double subtotal;
 
-        public void main(int cantidad,String id_producto,String nombre,Double precio,Double subtotal){
+        public productosTicket(int cantidad, String id_producto, String nombre, Double precio, Double subtotal) {
             this.cantidad=cantidad;
             this.id_producto=id_producto;
             this.nombre=nombre;
             this.precio=precio;
             this.subtotal=subtotal;
         }
-
-
-
 
     }
 
