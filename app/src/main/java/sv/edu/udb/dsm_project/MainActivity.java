81b21fa7617ae5277446b64039d6bd7c4164da39
angel <angelH98@outlook.com>
2021-04-25 11:02:11 -0600

    Fixing declarations

--- app/src/main/java/sv/edu/udb/dsm_project/MainActivity.java
+++ app/src/main/java/sv/edu/udb/dsm_project/MainActivity.java
@@ -2,6 +2,7 @@ package sv.edu.udb.dsm_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registro_activity);
+
+        Intent intent = new Intent(this, Registro_Menu.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/sv/edu/udb/dsm_project/Registro_Menu.java
+++ app/src/main/java/sv/edu/udb/dsm_project/Registro_Menu.java
@@ -2,6 +2,7 @@ package sv.edu.udb.dsm_project;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -9,8 +10,8 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -18,32 +19,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Registro_Menu extends AppCompatActivity {
+
     EditText Nomb,Descri,Precio;
     CheckBox Esta;
     Button btnGua;
     FirebaseFirestore db;
     private static final String TAG = "DocSnippets";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CrearDoc();
+
         Iniciar();
 
     }
     public void Iniciar()
     {
-        btnGua= findViewById(R.id.btnGua);
+        btnGua = (Button) findViewById(R.id.btnSend);
         Nomb= findViewById(R.id.txtNombre);
         Descri=findViewById(R.id.txtDescripcion);
         Precio=findViewById(R.id.txtPrecio);
         Esta=findViewById(R.id.chkEsta);
+
         db= FirebaseFirestore.getInstance();
+        CrearDoc();
     }
 
     public void CrearDoc()
     {
         // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
+        Map<String, Object> user = new HashMap<String, Object>();
         user.put("first", "Ada");
         user.put("last", "Lovelace");
         user.put("born", 1815);
@@ -51,16 +56,10 @@ public class Registro_Menu extends AppCompatActivity {
         // Add a new document with a generated ID
         db.collection("Prueba")
                 .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Log.d(TAG, "onComplete: ");
                     }
                 });
     }
