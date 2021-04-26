c1af4a1877d0a1f5c596f060bdb8ca224ec405f5
JLopzz <javylopzz@gmail.com>
2021-04-25 21:06:17 -0600

    carrito v0.1

--- app/src/main/java/sv/edu/udb/dsm_project/Carrito.java
+++ app/src/main/java/sv/edu/udb/dsm_project/Carrito.java
@@ -6,19 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.DecimalFormat;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.gson.Gson;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
@@ -29,6 +40,8 @@ public class Carrito extends AppCompatActivity {
     private String loginUser;
     DecimalFormat df= new DecimalFormat("#.00");
     private List<productosTicket> productos = new ArrayList<productosTicket>();
+    private TextView totapPagoTV;
+    private String actualDoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +51,20 @@ public class Carrito extends AppCompatActivity {
         loginUser="usuarioprueba123";
         mFirestore=FirebaseFirestore.getInstance();
         padre=(LinearLayout)findViewById(R.id.padre);
+        totapPagoTV = findViewById(R.id.totalPagar);
 
         obtenerDatos();
     }
 
     private void obtenerDatos(){
-        mFirestore.collection("tickets").whereEqualTo("id_usuario",loginUser).whereEqualTo("estado","Borrador").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        mFirestore.collection("tickets")
+            .whereEqualTo("id_usuario",loginUser)
+            .whereEqualTo("estado","Borrador")
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-
-
                     JSONParser parser = new JSONParser();
                     Gson gson = new Gson();
                     try{
@@ -59,6 +75,8 @@ public class Carrito extends AppCompatActivity {
                         String id_producto, nombre;
                         Double precio, subtotal;
                         for (QueryDocumentSnapshot document : task.getResult()) {
+
+                            actualDoc = document.getId();
 
                             obj = (JSONObject) parser.parse(gson.toJson(document.getData()));
                             array = (JSONArray) obj.get("productos");
@@ -78,11 +96,43 @@ public class Carrito extends AppCompatActivity {
                     }catch (Exception e){
                         Log.d("exception", e.toString());
                     }
-                    Log.d("TAG", "onComplete: ");
+                    Log.d("TAG", productos.toString());
                     datos(padre);
                 }
             }
         });
+    }
+
+    public void comprar(View view) {
+        mFirestore.collection("tickets")
+            .document(actualDoc).update(
+                "estado","Cancelado",
+                "fechapago", String.valueOf(new SimpleDateFormat("hh: mm: ss a dd-MMM-yyyy").format(new Date()))
+        );
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("estado", "Borrador");
+        data.put("idusuario", loginUser);
+        data.put("fechapago",null);
+        data.put("preciototal",0);
+
+        mFirestore.collection("tickets")
+                .add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.i("info",documentReference.getId());
+                        Toast.makeText(getApplicationContext(),"Compra realizada con exito",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),"Hubo un pequeño problema",Toast.LENGTH_SHORT).show();
+                        Log.i("info",e.toString());
+                    }
+                });
+
     }
 
     public class productosTicket{
@@ -105,35 +155,65 @@ public class Carrito extends AppCompatActivity {
 
     public void datos(LinearLayout padre){
         double total=0;
+
+//        LinearLayout.LayoutParams layParams = new LinearLayout.LayoutParams(width, height,weigth);
+
         for (int i=0;i<productos.size();i++){
-            TextView nombre= new TextView(getApplicationContext());
-            TextView cantidad= new TextView(getApplicationContext());
-            TextView precio= new TextView(getApplicationContext());
-            TextView subprecio= new TextView(getApplicationContext());
-            TextView producto= new TextView(getApplicationContext());
-            producto.setText("Producto "+(i+1));
+            //layouts para organizar la info
+            LinearLayout hijo = new LinearLayout(padre.getContext());
+            hijo.setPadding(20,5,5,5);
+            hijo.setOrientation(LinearLayout.HORIZONTAL);
+            hijo.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            //detalle de la info
+            LinearLayout detalles = new LinearLayout(padre.getContext());
+            detalles.setLayoutParams(new LinearLayout.LayoutParams(890, LinearLayout.LayoutParams.MATCH_PARENT));
+            detalles.setOrientation(LinearLayout.VERTICAL);
+
+            //agregar o quitar cantidad
+            LinearLayout acciones = new LinearLayout(padre.getContext());
+            acciones.setLayoutParams(new LinearLayout.LayoutParams(110,LinearLayout.LayoutParams.MATCH_PARENT));
+            acciones.setOrientation(LinearLayout.VERTICAL);
+
+            //textView que contiene la info
+            TextView nombre= new TextView(detalles.getContext());
+            TextView precio= new TextView(detalles.getContext());
+            TextView subprecio= new TextView(detalles.getContext());
             nombre.setText("Nombre: "+productos.get(i).nombre);
-            cantidad.setText("Cantidad: "+(int)productos.get(i).cantidad);
             precio.setText("Precio $"+productos.get(i).precio.toString());
-            subprecio.setText("Sub total $"+df.format(productos.get(i).precio*productos.get(i).cantidad)+"\n\n");
-            total=total+productos.get(i).precio*productos.get(i).cantidad;
-            padre.addView(producto);
-            padre.addView(nombre);
-            padre.addView(cantidad);
-            padre.addView(precio);
-            padre.addView(subprecio);
+            subprecio.setText("Sub total $"+df.format(productos.get(i).precio*productos.get(i).cantidad));
+            total+= productos.get(i).precio*productos.get(i).cantidad;
+            detalles.addView(nombre);
+            detalles.addView(precio);
+            detalles.addView(subprecio);
+            MoreLessButtons(acciones,(int)productos.get(i).cantidad);
+            hijo.addView(detalles);
+            hijo.addView(acciones);
+            padre.addView(hijo);
 
         }
-        TextView totalcompra= new TextView(getApplicationContext());
-        totalcompra.setText("Total de la compra = $"+df.format(total));
-        Button realizarCompra = new Button(getApplicationContext());
-        realizarCompra.setText("Realizar compra");
-        padre.addView(totalcompra);
-        padre.addView(realizarCompra);
+        totapPagoTV.setText("Total de la compra = $"+df.format(total));
     }
 
+    private void MoreLessButtons(LinearLayout v,int cant){
+        Button add = new Button(v.getContext());
+        add.setCompoundDrawablesWithIntrinsicBounds(R.drawable.more_one,0,0,0);
+        add.setLayoutParams(new LinearLayout.LayoutParams(100,100));
+        add.setPadding(0,4,16,4);
+        add.setLeft(0);
+        v.addView(add);
+        TextView cantidad = new TextView(v.getContext());
+        cantidad.setText(String.valueOf(cant));
+        v.addView(cantidad);
+        Button less = new Button(v.getContext());
+        less.setCompoundDrawablesWithIntrinsicBounds(R.drawable.more_one,0,0,0);
+        less.setLayoutParams(new LinearLayout.LayoutParams(100,100));
+        less.setPadding(0,4,16,4);
+        less.setLeft(0);
+        v.addView(less);
 
 
+    }
 
 }
 
